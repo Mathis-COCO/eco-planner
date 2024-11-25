ae3463baf8c2313b24dd6fdd06655b894ee7eb5e
MCOCO <mathis.coco@ynov.com>
2024-11-25 11:23:58 +0100

    working gas station api call

---
+++ eco-planner/types/Station.ts
@@ -0,0 +1,11 @@
+interface Station {
+    id: string;
+    name: string;
+    latitude: string;
+    longitude: string;
+    price: string;
+    brand: string;
+    address: string;
+    distance: string;
+  }
+  
